5196fb04b83e3a85fc13a4e9517d883fca1546ec
Stephen Cheng <aw3someone@gmail.com>
2017-11-02 14:12:38 -0700

    made properties public in Task.php and moved Entity.php to core folder

--- application/models/Task.php
+++ application/models/Task.php
@@ -1,12 +1,14 @@
 <?php
 
+require_once("./Entity.php");
+
 /* Represents a single task */
 class Task extends Entity {
     
-    $task;
-    $priority;
-    $size;
-    $group;
+    public $task;
+    public $priority;
+    public $size;
+    public $group;
     
     public function setTask($value) {
         $regex = "/^(\w){1,64}$/";
@@ -20,18 +22,24 @@ class Task extends Entity {
     public function setPriority($value) {
         if (is_int($value) && (0 < $value && $value < 4)) {
             $this -> priority = $value;
+        } else {
+            throw new Exception("Invalid Priority");
         }
     }
     
     public function setSize($value) {
         if (is_int($value) && (0 < $value && $value < 4)) {
             $this -> size = $value;
+        } else {
+            throw new Exception("Invalid Size");
         }
     }
     
     public function setGroup($value) {
         if (is_int($value) && (0 < $value && $value < 5)) {
             $this -> group = $value;    
+        } else {
+            throw new Exception("Invalid Group");
         }
     }
     
